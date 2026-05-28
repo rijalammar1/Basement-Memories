6baa15e9e66b33695b30e4d99db40abc69d8a367
rijalammar1 <rijaltn13@gmail.com>
2026-05-28 13:08:43 +0700

    feat: create login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/components/auth/LoginForm';
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-black px-6">
+    <main className="flex min-h-screen items-center justify-center bg-black px-7">
       <LoginForm />
     </main>
   );
