93d042412636867ba407e0479a4ec1033fe9c4e4
rijalammar1 <rijaltn13@gmail.com>
2026-05-28 13:07:05 +0700

    style: update global styling

---
+++ src/components/home/Topbar.tsx
@@ -0,0 +1,9 @@
+import { HiOutlineMoon, HiOutlineBell } from 'react-icons/hi';
+
+export default function Topbar() {
+  return (
+    <header className="sticky top-0 z-50 border-b border-zinc-900 bg-black/80 backdrop-blur-lg">
+      <div className="flex items-center justify-between px-6 py-4"></div>
+    </header>
+  );
+}
