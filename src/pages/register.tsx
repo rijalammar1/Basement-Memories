30e2e137624bf5e7abfcb173fc72ca03235bdac9
rijalammar1 <rijaltn13@gmail.com>
2026-05-28 13:10:33 +0700

    feat: create register  page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -2,7 +2,7 @@ import RegisterForm from '@/components/auth/RegisterForm';
 
 export default function RegisterPage() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-black px-6">
+    <main className="flex min-h-screen items-center justify-center bg-black px-7">
       <RegisterForm />
     </main>
   );
